4f003307b8b09fcb9c6ff235099988c71a0265c2
Zac Mueterthies <zjm@iastate.edu>
2025-02-11 11:14:41 -0600

    fixed opening new tab on redirect

--- coralx-frontend/app/page.tsx
+++ coralx-frontend/app/page.tsx
@@ -9,7 +9,7 @@ export default function LandingPage() {
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/login"
-            target="_blank"
+            //target="_blank"
             rel="noopener noreferrer"
           >
             Login
@@ -17,7 +17,7 @@ export default function LandingPage() {
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="/register"
-            target="_blank"
+            //target="_blank"
             rel="noopener noreferrer"
           >
             Register
