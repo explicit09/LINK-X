4a2e762710c6d094e7dc5aaca126818d9a2b144d
Zac Mueterthies <zjm@iastate.edu>
2025-02-13 21:01:30 -0600

    work on navbar

--- coralx-frontend/app/(landing)/page.tsx
+++ coralx-frontend/app/(landing)/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 
 export default function LandingPage() {
   return (
-  <div className="relative min-h-screen font-[family-name:var(--font-geist-sans)]">      
-      <header className="absolute top-0 left-0 w-full">
-        <nav className="px-4 lg:px-6 py-2.5">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="/" className="flex items-center">
-              <img src="/images/Logots.png" className="mr-3 h-6 sm:h-9" alt="Link-X Logo" />
+  <div className="relative min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <header className="absolute top-0 left-0 w-full h-[6.5vh] flex items-center">
+        <nav className="w-full px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center w-full h-full border-2">
+            <a href="/" className="flex items-center h-full">
+              <img
+                src="/images/Logots.png"
+                className="max-h-[6.5vh] object-contain"
+                alt="Link-X Logo"
+              />
             </a>
             <div className="flex items-center lg:order-2">
               <a href="/login" className="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
