e6b3b90b96edf57f1afb553cf9c4e5fba84649d1
parkscarter <143015603+parkscarter@users.noreply.github.com>
2025-02-20 12:51:49 -0600

    Added Dashboard Skeleton

--- coralx-frontend/app/(auth)/auth.config.ts
+++ coralx-frontend/app/(auth)/auth.config.ts
@@ -12,26 +12,31 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnChat = nextUrl.pathname.startsWith('/');
+      const isOnChat = nextUrl.pathname.startsWith('/chat');
       const isOnRegister = nextUrl.pathname.startsWith('/register');
       const isOnLogin = nextUrl.pathname.startsWith('/login');
+      const isOnLanding = nextUrl.pathname === '/';
 
-      if (isLoggedIn && (isOnLogin || isOnRegister)) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
+      // If user is not logged in and tries to access /, allow access to the landing page
+      if (!isLoggedIn && isOnLanding) return true;
+
+      // If user is logged in and tries to access /, /login, or /register redirect to /chat (in the future, this will be changesd to the user dashboard page)
+      if (isLoggedIn && (isOnLanding || isOnLogin || isOnRegister)) {
+        return Response.redirect(new URL('/chat', nextUrl));
       }
 
-      if (isOnRegister || isOnLogin) {
-        return true; // Always allow access to register and login pages
+      // Always allow access to register and login pages
+      if (isOnRegister || isOnLogin) return true; 
+
+      // If on /chat, only allow access if logged in, else redirect to login
+      if (isOnChat && !isLoggedIn) {
+        return Response.redirect(new URL('/login', nextUrl));
       }
 
-      if (isOnChat) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      }
-
-      if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
-      }
+      // For any other unauthorized access, redirect to /login
+      // if (!isLoggedIn) {
+      //   return Response.redirect(new URL('/login', nextUrl));
+      // }
 
       return true;
     },
