62ab529af9728b117bf56536a3fe6a0bbe92d8e0
Zac Mueterthies <zjm@iastate.edu>
2025-02-26 16:10:45 -0600

    changed sidebar to Link-X

--- coralx-frontend/components/app-sidebar.tsx
+++ coralx-frontend/components/app-sidebar.tsx
@@ -34,7 +34,7 @@ export function AppSidebar({ user }: { user: User | undefined }) {
           className="flex flex-row gap-3 items-center"
         >
           <span className="text-lg font-bold px-2 hover:bg-muted rounded-md cursor-pointer">
-            CoralX AI
+            Link-X
           </span>
         </Link>
         <Button
