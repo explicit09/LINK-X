e09c34f02ab202b15880265249d5a0c0f02e2769
Zac Mueterthies <zjm@iastate.edu>
2025-02-25 20:31:42 -0600

    header sign out works

--- coralx-frontend/components/link-x/Header.tsx
+++ coralx-frontend/components/link-x/Header.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { signOut } from "next-auth/react"
 // import { useTheme } from "next-themes"
 
 type HeaderProps = {
@@ -14,6 +16,18 @@ const Header = ({ showAuthButton = true, isLoggedIn }: HeaderProps) => {
   // const { resolvedTheme } = useTheme()
   // Select logo based on light or dark theme
   // const logoSrc = resolvedTheme === "dark" ? "/images/Logo-dark.png" : "/images/Logo-light.png"
+
+  const router = useRouter()
+
+  const handleSignOut = () => {
+    signOut({
+      redirectTo: "/",
+    })
+  }
+
+  const handleLogin = () => {
+    router.push("/login")
+  }
 
   return (
     <header className="absolute top-4 left-0 w-full h-[8vh] flex items-center">
@@ -32,14 +46,13 @@ const Header = ({ showAuthButton = true, isLoggedIn }: HeaderProps) => {
           </Link>
           <div className="flex items-center lg:order-2">
             {showAuthButton && (
-                <Link
-                    href={isLoggedIn ? "/logout" : "/login"}      // TODO: Define actual method for logging out later
-                    // className="hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                    // For now, use Dark mode styling across the board
-                    className="hover:bg-gray-700 focus:ring-4 focus:ring-gray-800 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                >
-                    {isLoggedIn ? "Log out" : "Log in"}
-                </Link>
+              <button
+                type="button"
+                className="w-full cursor-pointer hover:bg-gray-700 focus:ring-4 focus:ring-gray-800 rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                onClick={isLoggedIn ? handleSignOut : handleLogin}
+              >
+                {isLoggedIn ? "Sign out" : "Log in"}
+              </button>
             )}
           </div>
         </div>
