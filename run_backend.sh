b97d0ddac1a5966fc35f1a0ef6e504f18fcbd73c
Zac Mueterthies <zjm@iastate.edu>
2025-03-25 13:26:34 -0500

    Fixed merge mistake

--- run_backend.sh
+++ run_backend.sh
@@ -17,11 +17,15 @@ if $REMOVE_OLD; then
     docker ps -aq --filter "ancestor=dev7" | xargs -r docker rm -f
 fi
 
+# Step 1: Build Backend
+echo "Step 1: Building Backend..."
+docker build -t dev7 docker-image || { echo "Docker build failed"; exit 1; }
 
-# -d flag to run in detatched mode
-# use "docker exec -it <container_id_or_name> /bin/bash" 
-# to start an interactive shell not attached to the main process
+# Step 2: Run Backend
+echo "Step 2: Running Backend..."
+cd docker-image/src || { echo "Failed to navigate to docker-image/src"; exit 1; }
+
+# # -d flag to run in detatched mode
+# # use "docker exec -it <container_id_or_name> /bin/bash" 
+# # to start an interactive shell not attached to the main process
 docker run -d --env-file .env -p 8080:8080 dev7 || { echo "Docker run failed"; exit 1; }
-
-# Without -d flag
-# docker run --env-file .env -p 8080:8080 dev7 || { echo "Docker run failed"; exit 1; }
