75d22220bc1df2f8407681b35a93c85093bfe28e
Zac Mueterthies <zjm@iastate.edu>
2025-04-10 20:30:23 -0500

    queries & docker compose files

--- docker-image/src/queries.py
+++ docker-image/src/queries.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import json
 from item_04_retriever_FAISS import answer_to_QA
 
-def create_outline(working_dir):
-    query = """You are an AI assistant with access to provided content on a subject. Your task is to summarize this content into exactly 10 chapters. For each chapter:
+def generate_course_outline(working_dir):
+    query = ( 
+    """
+    You are an AI assistant with access to provided content on a subject. Your task is to summarize this content into exactly 10 chapters. For each chapter:
 
     1. Provide a concise title (3–7 words).
     2. Include an array of relevant metadata or key points. Each array should have 3–6 bullet items covering the main ideas, important highlights, and any important details from the content.
@@ -22,6 +25,26 @@ def create_outline(working_dir):
         },
         ...
     ]
-    }"""
+    }
+    """
+    )
 
     response = answer_to_QA(query, working_dir)
+
+    # try:
+    #     json.loads(response)
+    # except ValueError as e:
+        # do something if json is invalid
+    print(response)
+    return 0
+
+def generate_module_content():
+    # TODO
+    # Take faiss index, specific module json from outline (chapterTitle & metadata), persona
+    # Generate the content for the specified module using data provided & broader internet
+    return 0
+
+def valid_query(query):
+    # TODO
+    # Take a query given by a user and verify it is related to the course content
+    return 0
