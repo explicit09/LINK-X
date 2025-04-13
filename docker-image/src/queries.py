45f2d917abd8cf6ce6215cb9b70db00244704ae8
Zac Mueterthies <zjm@iastate.edu>
2025-04-13 16:54:12 -0500

    LLM Queries

--- docker-image/src/queries.py
+++ docker-image/src/queries.py
@@ -3,7 +3,7 @@ import sys
 import json
 from item_04_retriever_FAISS import answer_to_QA
 
-def generate_course_outline(working_dir):
+def generate_course_outline_RAG(working_dir):
     query = ( 
     """
     You are an AI assistant with access to provided content on a subject. Your task is to summarize this content into exactly 10 chapters. For each chapter:
@@ -31,17 +31,60 @@ def generate_course_outline(working_dir):
 
     response = answer_to_QA(query, working_dir)
 
-    # try:
-    #     json.loads(response)
-    # except ValueError as e:
-        # do something if json is invalid
-    print(response)
-    return 0
+    try:
+        parsed_json = json.loads(response)
+    except ValueError as e:
+        print("Invalid JSON returned from AI response.")
+        print("Error:", e)
+        return None
+
+    # return response
+    return parsed_json
+
+def generate_course_outline(user_query):
+    # “I’m a sophomore in finance and I want to learn about investing”
+    query = ( 
+    f"""
+    You are an AI assistant generating an educational course.  You have been provided with a user's learning goals: "{user_query}".  Your task is to retrieve all relevant content and summarize it into exactly 10 chapters. For each chapter:
+
+    1. Provide a concise title (3–7 words).
+    2. Include an array of relevant metadata or key points. Each array should have 3–6 bullet items covering the main ideas, important highlights, and any important details from the content.
+
+    Return the results as valid JSON, with the following structure:
+
+    {{
+    "chapters": [
+        {{
+        "chapterTitle": "string",
+        "metadata": [
+            "string",
+            "string",
+                ...
+            ]
+        }},
+        ...
+    ]
+    }}
+    """
+    )
+        
+    response = answer_to_QA(query, None)
+
+    try:
+        parsed_json = json.loads(response)
+    except ValueError as e:
+        print("Invalid JSON returned from AI response.")
+        print("Error:", e)
+        return None
+
+    # return response
+    return parsed_json
 
 def generate_module_content():
     # TODO
     # Take faiss index, specific module json from outline (chapterTitle & metadata), persona
     # Generate the content for the specified module using data provided & broader internet
+    # "Based on JSON, generate all relevant knowledge"
     return 0
 
 def valid_query(user_query, working_dir):
@@ -65,6 +108,5 @@ def valid_query(user_query, working_dir):
     """
     )
     response = answer_to_QA(query, working_dir)
-    print(response)
-
-    return 0
+    # print(response)
+    return response
