7739f18aa54708181ef9ed7fc66da599f2745abf
morgapri <morganprieskorn35@gmail.com>
2025-04-28 13:44:57 -0500

    changes to dashboard

--- coralx-frontend/app/(dash)/dashboard/page.tsx
+++ coralx-frontend/app/(dash)/dashboard/page.tsx
@@ -9,6 +9,7 @@ import Sidebar from "@/components/link-x/DashSidebar";
 import StatisticsCard from "@/components/dashboard/StatisticsCard";
 import MarketTrends from "@/components/dashboard/MarketTrends";
 import CoursesList from "@/components/dashboard/CoursesList";
+import CoursesGrid from "@/components/dashboard/CoursesGrid";
 import AudioUpload from "@/components/dashboard/AudioUpload";
 import RecentlyCompletedCourses from "@/components/dashboard/RecentCourses";
 import Header from "@/components/link-x/Header";
@@ -36,21 +37,14 @@ export default function Dashboard() {
             Welcome back to Learn-X! Here's an overview of your  learning journey.
           </h2>
           
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-8">
-            <StatisticsCard title="Courses Completed" value="12" icon={BookOpen} />
-            <StatisticsCard title="Hours Studied" value="87" icon={Clock} />
-            <StatisticsCard title="Community Rank" value="#42" icon={TrendingUp} />
-            <StatisticsCard title="Next Milestone" value="15 courses" icon={GraduationCap} />
-          </div>
+         
 
           <div className="grid grid-cols-1 gap-6">
             <LearnPrompt />
-            <CoursesList search={search} setSearch={setSearch} />
+            <CoursesGrid search={search} setSearch={setSearch} />
           </div>
 
-          <div className="grid grid-cols-1 gap-6 my-8">
-            <RecentlyCompletedCourses />
-          </div>
+          
           <div className="grid grid-cols-1 gap-6 my-8">
             <AudioUpload />
           </div>
